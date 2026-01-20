d3570ea8c04a255b73373c3056f8aaf8ce745bd1
Kanwal <gurkanwalsingh04@gmail.com>
2026-01-20 17:17:17 +0530

    Force Postgres connection for migrations

--- ptat/apps.py
+++ ptat/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class PtatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ptat'
+
+    def ready(self):
+        try:
+            from django.db import connection
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1")
+        except Exception as e:
+            print("DB not ready:", e)
